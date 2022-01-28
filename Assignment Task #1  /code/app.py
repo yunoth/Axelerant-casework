470a11da2e40f0df72f326a1d1eaf573da9a2f8f
Yunoth kumar <yunothkumar_m@trimble.com>
2022-01-28 16:04:54 +0530

    Initial commit

--- Assignment Task #1  /code/app.py
+++ Assignment Task #1  /code/app.py
@@ -1,8 +1,7 @@
+import socket
 from flask import Flask
 app = Flask(__name__)
-import socket
 
 @app.route('/')
 def index():
-    resp="response from container id: "+socket.gethostname()
-    return str(resp)
+    return "response from container id: "+socket.gethostname()
