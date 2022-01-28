601020f67f0e904909165f367a39bb00104f30ed
Yunoth kumar <yunothkumar_m@trimble.com>
2022-01-29 00:35:43 +0530

    Initial commit

---
+++ Assignment Task #2 /demo-infra/security_group.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "alb-sg" {
+  name        = "alb-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+  tags = {
+    Name = "alb-sg"
+  }
+}
+resource "aws_security_group" "instance-sg" {
+  name        = "instance-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "instance-sg"
+  }
+}
+resource "aws_security_group" "db-sg" {
+  name        = "db-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "db-sg"
+  }
+}
+resource "aws_security_group_rule" "alb-rule" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb-sg.id
+}
+resource "aws_security_group_rule" "instance-rule" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.alb-sg.id
+  security_group_id = aws_security_group.instance-sg.id
+}
+resource "aws_security_group_rule" "db-rule" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.instance-sg.id
+  security_group_id = aws_security_group.db-sg.id
+}
---
+++ Assignment Task #2 /demo-infra/userdata.tpl
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /opt
+wget https://ftp.drupal.org/files/projects/drupal-8.9.20.tar.gz
+tar -xvf drupal-8.9.20.tar.gz
