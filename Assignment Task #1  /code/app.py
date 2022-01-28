20018b3bef0870ef3eda419c6ac29c3b92d6491d
Yunoth kumar <yunothkumar_m@trimble.com>
2022-01-28 18:06:43 +0530

    Initial commit

--- Assignment Task #1  /code/app.py
+++ Assignment Task #1  /code/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "response from container id: "+socket.gethostname()
+    return "response from container id: "+socket.gethostname()+"\n"
