d13c4bc5e8a1847cfd45af5654537364bfb0dcb9
Yunoth kumar <yunothkumar_m@trimble.com>
2022-01-28 15:52:03 +0530

    Initial commit

--- Assignment Task #1  /code/app.py
+++ Assignment Task #1  /code/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+import socket
+#docker_short_id = socket.gethostname()
 
 @app.route('/')
 def index():
-    return 'hello world!'
+    return socket.gethostname()
     #return request.headers.get('Host').split(':')[0]
